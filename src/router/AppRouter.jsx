ac0c25692628b435c1d769af0bb4b29bd14376b8
Mahir <mkojak75@gmail.com>
2024-09-28 00:57:25 +0200

    user

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
-import Navbar from "../components/Navbar";
+
 import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
-    // <BrowserRouter>
+    
     <>
-      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="dashboard" element={<PrivateRouter />}>
@@ -27,7 +27,7 @@ const AppRouter = () => {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-    // </BrowserRouter>
+    
   );
 };
 
